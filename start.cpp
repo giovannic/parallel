65851f45bb646829c3c3b62bd2fec8c514793f7f
Łukasz Koprowski <azram19@gmail.com>
2013-11-23 23:15:29 +0000

    Comments about data dependencies and possible refactorings to maximize parallelizm

--- start.cpp
+++ start.cpp
@@ -36,27 +36,88 @@ double Euler(double T) {
   double H = M_PI/T;
 
   double Sum = LReal(X,0)/2;
+  
   for(int N = 1; N <= Ntr; N++) {
     double Y = N*H;
     Sum += pow((-1),N)*LReal(X,Y);
   }
 
+  // Can be refactored to Map -> Reduce
+  //
+  // #1 Map - fully independent
+  // PS1[Ntr+1];
+  // for(int N = 1; N <= Ntr; N++){
+  //	double Y = N*H;
+  // 	PS1[N] = poq((-1), N)*LReal(X, Y);
+  // }
+  //
+  // #2 Reduce - sequential
+  // Sum = sum(PS1);
+
   SU[0] = Sum;
+  
   for(int K = 0; K < 12; K++) {
     int N = Ntr + K+1;
     double Y = N*H;
     SU[K+1] = SU[K] + pow((-1), N)*LReal(X,Y);
   }
-
+  
+  //Can be refactored to
+  //
+  // #1 Map - fully independent, each run only needs to know H and X
+  //
+  // double PS[13]
+  // for(int K = 0; K < 12; K++){
+  //	int N = Ntr + K + 1;
+  //	double Y = N * H;
+  //	PS[K] = pow((-1), N) * LReadl(X,Y);
+  // }
+  //
+  // #2 Sequential Reduce - fully sequential
+  // #3 Parallel - could be turned into fully independent by
+  // distributing PS and Sum to all of the process and collecting the result:
+  // SU[1] = Sum + PS[0]
+  // SU[2] = Sum + PS[0] + PS[1]
+  // SU[3] = Sum + PS[0] + PS[1] + PS[2]
+  // 
+  // for(int K = 0; K < 12; k++){
+  //  SU[K+1] = SU[K] + PS[K];
+  // }
+  
+  // This is where computations of Fun and Fun1 could be done independantly
+  // They could diverge onto two different thingies
   double Avgsu = 0;
   double Avgsu1 = 0;
+  
   for(int j = 0; j < 12; j++) {
     Avgsu += C[j]*SU[j];
     Avgsu1 += C[j]*SU[j+1];
   }
 
+  // Can be refactored to
+  //
+  // CS[2][13]
+  //
+  // # Fully independent loop - map
+  // for(int j = 0; j < 12; j++){
+  // 	CS[0][j] = C[j] * SU[j];
+  // }
+  //
+  // # Fully independent loop - map
+  // # Each process requires to know only C[j] and Su[j+1]
+  // for(int j = 0; j < 12; j++){
+  //	CS[1][j] = C[j]*SU[j+1];
+  // }
+  //
+  // # Reduce - fuly sequential
+  // Avgsu = sum(CS[0]);
+  // Avgsu1 = sum(CS[1])
+  
+  
   double Fun = U*Avgsu/2048;
   double Fun1 = U*Avgsu1/2048;
+  
+  // This is where Fun and Fun1 have to converge
   double err = abs(Fun-Fun1)/2;
 
   return Fun1;
