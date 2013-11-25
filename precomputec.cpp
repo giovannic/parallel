8e3982f52076f6b02d10665b454c19ee3a6f4c59
Lukasz Koprowski <azram19@gmail.com>
2013-11-25 14:38:13 +0000

    Script precomputing coeficcients

---
+++ precomputec.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+unsigned long long choose(unsigned n, unsigned k){
+  unsigned long long r = 1;
+  if(k > n) return 0;
+  for(unsigned d=1;d<=k;d++){
+    r *= n--;
+    r /= d;
+  }
+
+  return r;
+}
+
+int main(){
+  int k;
+  cin >> k;
+  cout << "unsigned long long C[] = {";
+  unsigned long long t = 1;
+  for(int i = 0; i <= k; i++){
+    cout << choose(k, i) << ", ";
+  }
+  cout << "};";
+}
+
